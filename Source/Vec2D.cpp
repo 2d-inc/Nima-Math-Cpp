faab42f0d1b63e1df73f5c69944ac5ff9c6ca6e2
Luigi Rosso <luigi.rosso@gmail.com>
2018-08-22 16:07:25 -0700

    Adding distance and negate methods.

--- Source/Vec2D.cpp
+++ Source/Vec2D.cpp
@@ -51,7 +51,14 @@ float Vec2D::length(const Vec2D& a)
 {
 	float x = a[0];
 	float y = a[1];
-	return sqrt(x*x + y*y);
+	return std::sqrt(x*x + y*y);
+}
+
+float Vec2D::distance(const Vec2D& a, const Vec2D& b)
+{
+	float x = b[0] - a[0];
+	float y = b[1] - a[1];
+	return std::sqrt(x*x + y*y);
 }
 
 void Vec2D::copy(Vec2D& result, const Vec2D& a)
@@ -90,4 +97,10 @@ void Vec2D::scale(Vec2D& o, const Vec2D& a, float scale)
 {
 	o[0] = a[0] * scale;
 	o[1] = a[1] * scale;
+}
+
+void Vec2D::negate(Vec2D& o, const Vec2D& a)
+{
+	o[0] = -a[0];
+	o[1] = -a[1];
 }
--- Source/Vec2D.hpp
+++ Source/Vec2D.hpp
@@ -25,11 +25,13 @@ namespace nima
 			static void subtract(Vec2D& result, const Vec2D& a, const Vec2D& b);
 			static void add(Vec2D& result, const Vec2D& a, const Vec2D& b);
 			static float length(const Vec2D& a);
+			static float distance(const Vec2D& a, const Vec2D& b);
 			static void copy(Vec2D& result, const Vec2D& a);
 			static void normalize(Vec2D& result, const Vec2D& a);
 			static float dot(const Vec2D& a, const Vec2D& b);
 			static void lerp(Vec2D& o, const Vec2D& a, const Vec2D& b, float f);
 			static void scale(Vec2D& o, const Vec2D& a, float scale);
+			static void negate(Vec2D& o, const Vec2D& a);
 	};
 
 	inline Vec2D operator*(const Mat2D& a, const Vec2D& b)
